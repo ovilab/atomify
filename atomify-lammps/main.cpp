4ef1bed9f09966beaa0781c5f0be5ea70e54645a
Svenn-Arne Dragly <s@dragly.com>
2015-04-20 11:41:57 +0200

    Added locale C on Linux

--- atomify-lammps/main.cpp
+++ atomify-lammps/main.cpp
@@ -2,6 +2,10 @@
 #include <QQmlApplicationEngine>
 #include <QtQml>
 
+#ifdef Q_OS_LINUX
+#include <locale>
+#endif
+
 #include "mysimulator.h"
 
 int main(int argc, char *argv[])
@@ -13,5 +17,9 @@ int main(int argc, char *argv[])
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
+#ifdef Q_OS_LINUX
+    setlocale(LC_ALL, "C");
+    setlocale(LC_NUMERIC, "C");
+#endif
     return app.exec();
 }
