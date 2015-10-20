8308f25df028f6febeb29528cd0299ae54fe7406
Anders Hafreager <anders@hafreager.no>
2015-10-20 14:09:56 +0200

    Removed unimportant QOpenGL stuff.

--- atomify-lammps/main.cpp
+++ atomify-lammps/main.cpp
@@ -22,26 +22,11 @@ int main(int argc, char *argv[])
     qmlRegisterType<CPCompute>("Compute", 1, 0, "Compute");
     qmlRegisterType<LammpsOutput>("LammpsOutput", 1, 0, "LammpsOutput");
     qmlRegisterType<AtomStyle>("AtomStyle", 1, 0, "AtomStyle");
-    // qmlRegisterType<FileIO>("Atomify", 1, 0, "FileIO");
     QApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
 
-    QSurfaceFormat format;
-    format.setProfile(QSurfaceFormat::CoreProfile);
-    format.setMajorVersion(4);
-    format.setMinorVersion(3);
-
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-    for(auto &obj : engine.rootObjects()) {
-        qDebug() << "Obj: " << obj;
-        qDebug() << "Window type: " << obj->isWindowType();
-
-        QQuickWindow *window = qobject_cast<QQuickWindow*>(obj);
-        if(window != NULL) {
-            window->setFormat(format);
-        } else qDebug() << "No :(((";
-    }
 #ifdef Q_OS_LINUX
     setlocale(LC_ALL, "C");
     setlocale(LC_NUMERIC, "C");
