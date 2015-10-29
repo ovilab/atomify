7098c64dec7284b29199f4f46d9c774d46c4c31f
Svenn-Arne Dragly <s@dragly.com>
2015-10-29 17:23:24 +0100

    Add back missing fix files

---
+++ atomify-lammps/cpfix.cpp
@@ -0,0 +1,40 @@
+#include "cpfix.h"
+#include "lammpscontroller.h"
+
+CPFix::CPFix(QObject *parent) : SimulatorControl(parent)
+{
+
+}
+
+void CPFix::update(LAMMPSController *lammpsController)
+{
+    SimulatorControl::update(lammpsController);
+
+}
+
+QList<QString> CPFix::enabledCommands()
+{
+    return {QString("fix %1 %2").arg(identifier()).arg(command())};
+}
+
+QList<QString> CPFix::disableCommands()
+{
+    return {QString("unfix %1 ").arg(identifier())};
+}
+
+void CPFix::updateCommand()
+{
+
+}
+
+bool CPFix::existsInLammps(LAMMPSController *lammpsController)
+{
+    LAMMPS_NS::Fix *fix = lammpsController->findFixByIdentifier(identifier());
+    return fix!=nullptr;
+}
+
+
+QList<QString> CPFix::resetCommands()
+{
+    return { QString("unfix %1").arg(identifier()), QString("fix %1 %2").arg(identifier()).arg(command()) };
+}
