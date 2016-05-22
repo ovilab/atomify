fcebdf028984b88a9b4c46abc9ded076291140a3
Anders Hafreager <andershaf@gmail.com>
2016-05-22 22:12:04 +0200

    Added (not implemented) Slice modifier.

---
+++ atomify-lammps/LammpsContainers/modifiers/slice.cpp
@@ -0,0 +1,53 @@
+#include "slice.h"
+
+Slice::Slice()
+{
+
+}
+
+
+void Slice::apply(AtomData &atomData)
+{
+}
+
+float Slice::distance() const
+{
+    return m_distance;
+}
+
+QVector3D Slice::normal() const
+{
+    return m_normal;
+}
+
+float Slice::width() const
+{
+    return m_width;
+}
+
+void Slice::setDistance(float distance)
+{
+    if (m_distance == distance)
+        return;
+
+    m_distance = distance;
+    emit distanceChanged(distance);
+}
+
+void Slice::setNormal(QVector3D normal)
+{
+    if (m_normal == normal)
+        return;
+
+    m_normal = normal;
+    emit normalChanged(normal);
+}
+
+void Slice::setWidth(float width)
+{
+    if (m_width == width)
+        return;
+
+    m_width = width;
+    emit widthChanged(width);
+}
