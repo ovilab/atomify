78208520d56d4adb038ce50938d5a8072767ec9e
Anders Hafreager <andershaf@gmail.com>
2016-05-30 09:37:28 +0200

    Removed debug output.

--- atomify-lammps/mysimulator.cpp
+++ atomify-lammps/mysimulator.cpp
@@ -60,12 +60,9 @@ void MyWorker::synchronizeSimulator(Simulator *simulator)
         emit atomifySimulator->lammpsDidReset();
     }
 
-    QElapsedTimer t;
-    t.start();
     atomifySimulator->scriptHandler()->setAtoms(atomifySimulator->system()->atoms());
     atomifySimulator->system()->synchronize(m_lammpsController.lammps());
     atomifySimulator->system()->atoms()->updateData(m_lammpsController.lammps());
-    qDebug() << "Data syncing took " << t.elapsed() << " ms.";
 
     // Sync values from QML and simulator
     m_lammpsController.setPaused(atomifySimulator->paused());
@@ -131,10 +128,7 @@ void MyWorker::synchronizeSimulator(Simulator *simulator)
 
 void MyWorker::work()
 {
-    QElapsedTimer t;
-    t.start();
     m_lammpsController.tick();
-    qDebug() << "Tick took " << t.elapsed() << " ms.";
     auto dt = m_elapsed.elapsed();
     double delta = 16 - dt;
     if(delta > 0) {
