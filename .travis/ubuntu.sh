35f25d6ebed5c84bfce0721c29dc4bc0bbaef21f
Svenn-Arne Dragly <s@dragly.com>
2017-05-02 14:16:17 +0200

    Use 5.9 instead of 5.7

--- .travis/ubuntu.sh
+++ .travis/ubuntu.sh
@@ -3,7 +3,7 @@ sudo apt-get update -qq
 sudo apt-get install -qq curl p7zip-full git
 sudo apt-get -qq build-dep qt5-default
 bash .travis/qt5-ubuntu.sh $(pwd) extra-charts 3d base declarative graphicaleffects imageformats multimedia quickcontrols quickcontrols2 sensors svg tools translations xmlpatterns
-export QTDIR=$(pwd)/5.7/gcc_64/
+export QTDIR=$(pwd)/5.9/gcc_64/
 export PATH=$QTDIR/bin:$PATH
 qmake
 make
