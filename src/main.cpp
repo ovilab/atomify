5ba1c9bb39ba39d78efa629dba07b6c5b77c0a15
Anders Hafreager <andershaf@gmail.com>
2017-05-05 18:48:21 +0200

    Fixing app icon on mac.

--- src/main.cpp
+++ src/main.cpp
@@ -167,8 +167,10 @@ int main(int argc, char *argv[])
         engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 #ifdef Q_OS_MAC
         QWindow *window = qobject_cast<QWindow*>(engine.rootObjects()[0]);
-        window->setIcon(QIcon(":/images/atomify_logo.icns"));
-        app.setWindowIcon(QIcon(":/images/atomify_logo.icns"));
+//        window->setIcon(QIcon(":/images/atomify_logo.icns"));
+//        app.setWindowIcon(QIcon(":/images/atomify_logo.icns"));
+        window->setIcon(QIcon("../Resources/icon.icns"));
+        app.setWindowIcon(QIcon("../Resources/icon.icns"));
 #endif
 #ifdef Q_OS_LINUX
         setlocale(LC_ALL, "C");
