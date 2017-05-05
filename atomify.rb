d6a784c8a00d5d7d18a908035814b65d47ca9dfa
Anders Hafreager <andershaf@gmail.com>
2017-05-05 17:33:10 +0200

    Updated version in brew file

--- atomify.rb
+++ atomify.rb
@@ -2,7 +2,7 @@ class Atomify < Formula
   desc "Live LAMMPS visualizer"
   homepage "http://ovilab.net/atomify"
   url "https://github.com/ovilab/atomify/releases/download/v2.0.6/atomify.tar.gz"
-  version "2.0"
+  version "v2.0.6"
   sha256 "10ecc1209b51142ba13868477b0f53107c1c2b44eae75312c7a2f35d225ffd33"
   head "https://github.com/ovilab/atomify.git", :using => :git, :branch => :dev
   depends_on "qt"
