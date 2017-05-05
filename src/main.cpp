969ec1ed09875bf5e9b5cb29ff81fb5b70581455
Anders Hafreager <andershaf@gmail.com>
2017-05-05 15:06:28 +0200

    --version uses printf()

--- src/main.cpp
+++ src/main.cpp
@@ -150,7 +150,7 @@ int main(int argc, char *argv[])
         }
 
         if(strcmp(argv[1], "--version")==0) {
-            qDebug() << "2.0";
+            printf("2.0");
             exit(0);
         }
     }
