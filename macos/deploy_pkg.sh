442da8d5f6dea7c68cb9b370c23eecc552aa3788
Anders Hafreager <andershaf@gmail.com>
2017-05-12 18:33:47 +0200

    Improved mac deployment scripts

--- macos/deploy_pkg.sh
+++ macos/deploy_pkg.sh
@@ -5,17 +5,13 @@
 # Create icons with
 # iconutil -c icns icon.iconset
 # sudo installer -store -pkg Atomify.pkg -target /
-mkdir app_store
-rm -rf app_store/*
-cp -r Atomify.app app_store
-cd app_store
+mkdir pkg
+rm -rf pkg/*
+cp -r Atomify.app pkg
+cd pkg
 xattr -cr Atomify.app
 ~/Qt/5.9/clang_64/bin/macdeployqt Atomify.app -qmldir=/projects/atomify/editor/atomify/src/qml -codesign="3rd Party Mac Developer Application: Anders Hafreager" -appstore-compliant
 cd "Atomify.app"
 find . -name *.dSYM | xargs -I $ rm -R $
 cd ..
-cp ../Atomify.app/Contents/MacOS/atomify Atomify.app/Contents/MacOS/
-codesign -s "3rd Party Mac Developer Application: Anders Hafreager" --entitlements /projects/atomify/editor/atomify/macos/entitlements.plist Atomify.app
-productbuild --component Atomify.app /Applications --sign "3rd Party Mac Developer Installer: Anders Hafreager" Atomify.pkg
-~/Qt/5.9/clang_64/bin/macdeployqt Atomify.app -qmldir=/projects/atomify/editor/atomify/src/qml -codesign="3rd Party Mac Developer Application: Anders Hafreager" -appstore-compliant
 productbuild --component Atomify.app /Applications --sign "3rd Party Mac Developer Installer: Anders Hafreager" Atomify.pkg
