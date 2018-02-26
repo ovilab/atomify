487e8ccde72bd13b8830300f7f7d7fa1b5df1c23
Anders Hafreager <andershaf@gmail.com>
2018-02-25 16:27:49 -0800

    Removed debug output

--- src/rendercapturehelper.cpp
+++ src/rendercapturehelper.cpp
@@ -9,14 +9,10 @@ RenderCaptureHelper::RenderCaptureHelper(QObject *parent) : QObject(parent)
 
 int RenderCaptureHelper::particleAtPoint(Qt3DRender::QRenderCaptureReply *captureReply, const QPointF &point)
 {
-    qDebug() << "Image" << captureReply->image();
     const QColor color = captureReply->image().pixelColor(point.toPoint());
-    qDebug() << "Mouse: " << point;
-    qDebug() << "Color" << color << color.redF() << color.greenF() << color.blueF() <<  color.red() << color.green() << color.blue();
-    float number = 255.0 * (color.redF() * 255 * 255 + color.greenF() * 255 + color.blueF());
-    qDebug() << "Number" << number;
-    float number2 = color.red() * 255 * 255 + color.green() * 255 + color.blue();
-    qDebug() << "Numbera" << number2;
+
+    int number = static_cast<int>(255.0 * (color.redF() * 255 * 255 + color.greenF() * 255 + color.blueF()));
+    // float number2 = color.red() * 255 * 255 + color.green() * 255 + color.blue();
     return number;
 }
 
